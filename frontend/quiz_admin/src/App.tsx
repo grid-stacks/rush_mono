be4e5d5190f8b21593b2ab9c81576e243228102c
DHN Chandan <php.chandan@gmail.com>
2022-05-09 23:25:40 +0600

    Test prettier

---
+++ frontend/quiz_admin/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Thing } from "quiz_components";
+
+function App() {
+	return (
+		<div className="App">
+			<Thing />
+			<h1>Test header</h1>
+		</div>
+	);
+}
+
+export default App;
